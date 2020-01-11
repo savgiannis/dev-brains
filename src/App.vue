0f81254847de9c1d269c61e2070f8942ec5e6327
Giannis <savvigiannhs@gmail.com>
2020-01-11 12:33:22 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,110 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <v-navigation-drawer fixed mini-variant>
+      <div class="nav d-flex flex-column align-center">
+        <v-tooltip right nudge-right="25" content-class="tooltip">
+          <template v-slot:activator="{ on }">
+            <v-icon class="active" large v-on="on">home</v-icon>
+          </template>
+          <span class="font-weight-bold">Home</span>
+        </v-tooltip>
+
+        <v-tooltip right nudge-right="25">
+          <template v-slot:activator="{ on }">
+            <v-icon large v-on="on">people</v-icon>
+          </template>
+          <span class="font-weight-bold">About</span>
+        </v-tooltip>
+
+        <v-tooltip right nudge-right="25">
+          <template v-slot:activator="{ on }">
+            <v-icon large v-on="on">work</v-icon>
+          </template>
+          <span class="font-weight-bold">Portfolio</span>
+        </v-tooltip>
+
+        <v-tooltip right nudge-right="25">
+          <template v-slot:activator="{ on }">
+            <v-icon large v-on="on">mail</v-icon>
+          </template>
+          <span class="font-weight-bold">Contact</span>
+        </v-tooltip>
+      </div>
+    </v-navigation-drawer>
+
+    <div class="background-image">
+      <v-overlay opacity="0.8"></v-overlay>
     </div>
-    <router-view/>
-  </div>
+
+    <v-content>
+      <div class="d-flex justify-center">
+        <div class="wrapper">
+          <Home />
+          <About />
+        </div>
+      </div>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+import Home from "@/views/Home";
+import About from "@/views/About";
+export default {
+  components: {
+    Home,
+    About
+  },
+  data() {
+    return {
+      test: ""
+    };
+  }
+};
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.background-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  background-image: url("assets/bg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
-#nav {
-  padding: 30px;
+.wrapper {
+  width: 1170px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.nav {
+  padding-top: 132px;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  & > *:not(:last-child) {
+    margin-bottom: 32px;
   }
+}
+
+.v-navigation-drawer {
+  background-color: rgba($primary, 0.85) !important;
+}
+
+.v-icon {
+  color: $secondary !important;
+  cursor: pointer;
+
+  &.active {
+    color: $node-light !important;
+  }
+}
+
+.v-tooltip__content {
+  background-color: $node-light !important;
+  color: $primary !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="wrapper d-flex flex-column justify-center align-center">
+      <h1 class="display-2 font-weight-bold white--text text-center mb-8">Hello! We are Dev Brains</h1>
+
+      <div class="headline white--text mb-1">We create</div>
+
+      <div
+        class="headline white--text mb-1"
+      >web/mobile/desktop applications, e-commerce platforms and CMS websites</div>
+
+      <div class="headline white--text mb-8">with</div>
+
+      <div class="d-flex align-center justify-center">
+        <img class="icon heart mr-4" src="@/assets/heart.svg" />
+        <div class="headline white--text mr-4">and</div>
+        <img class="icon mr-4" src="@/assets/js.svg" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+export default {};
+</script>
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+}
+
+.icon {
+  height: 70px;
+
+  &.heart {
+    filter: invert(10%) sepia(66%) saturate(7380%) hue-rotate(358deg)
+      brightness(98%) contrast(81%);
   }
 }
-</script>
+</style>
